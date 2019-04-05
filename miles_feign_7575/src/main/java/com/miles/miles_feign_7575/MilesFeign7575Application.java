87d39bc17dba11adf946a255e972d2ecd085ea4a
ChinTingKun <awalsmile@qq.com>
2019-04-05 20:16:44 +0800

    hystrix dashboard 相关

--- miles_feign_7575/src/main/java/com/miles/miles_feign_7575/MilesFeign7575Application.java
+++ miles_feign_7575/src/main/java/com/miles/miles_feign_7575/MilesFeign7575Application.java
@@ -1,10 +1,13 @@
 package com.miles.miles_feign_7575;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.miles")
 @EnableEurekaClient
@@ -15,6 +18,14 @@ public class MilesFeign7575Application {
 
     public static void main(String[] args) {
         SpringApplication.run(MilesFeign7575Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServletRgsterBean() {
+        ServletRegistrationBean<HystrixMetricsStreamServlet> bean = new ServletRegistrationBean<>();
+        bean.setServlet(new HystrixMetricsStreamServlet());
+        bean.addUrlMappings("/hystrix.stream");
+        return bean;
     }
 
 }
