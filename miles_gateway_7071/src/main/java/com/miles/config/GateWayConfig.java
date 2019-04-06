76cea537e3c6c50b9275d14752c6d1c2cd601964
ChinTingKun <awalsmile@qq.com>
2019-04-06 18:27:12 +0800

    gateway 自定义Filter

--- miles_gateway_7071/src/main/java/com/miles/config/GateWayConfig.java
+++ miles_gateway_7071/src/main/java/com/miles/config/GateWayConfig.java
@@ -3,6 +3,8 @@ package com.miles.config;
 import com.miles.filter.MGateWayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Resource;
 
@@ -13,47 +15,69 @@ import javax.annotation.Resource;
  * @Date 2019/4/6 12:07
  * @Version 1.0
  */
-//@Configuration
+@Configuration
 public class GateWayConfig {
 
     @Resource
     RouteLocatorBuilder builder;
 
-//    @Bean // 第一个route 路由ribbon 第二路由feign
+    @Bean  // 第一个route 路由ribbon 第二路由feign
     public RouteLocator getRouteLocator() {
         return builder.routes()
-                // ribbon
                 .route(ribbon -> ribbon.path("/ribbon/**")
-                        .uri("lb://M-COMSUMER-RIBBON").id("m-ribbon-one"))
-                //feign
-                .route(feign -> feign.path("/feign/**").filters(a -> a.stripPrefix(1)
-                        .filter(new MGateWayFilter()))
-                        .uri("lb://M-COMSUMER-FEIGN").id("m-feign-two"))
-                .build();
+                        .uri("lb://M-COMSUMER-RIBBON")
+                        .id("m-ribbon"))
+                .route(feign -> feign.path("/feign/**")
+                        .filters(feignF -> feignF.stripPrefix(1)
+                            .filter(new MGateWayFilter()))
+                        .uri("lb://M-COMSUMER-FEIGN")
+                        .id("m-feign-one")
+                ).build();
+
     }
-//    @Bean
+}
+
+////    @Resource
+////    @Autowired
+////    RouteLocatorBuilder builder;
+//
+//    @Bean // 第一个route 路由ribbon 第二路由feign
 //    public RouteLocator getRouteLocator(RouteLocatorBuilder builder) {
 //        return builder.routes()
-//                .route(new Function<PredicateSpec, Route.AsyncBuilder>() {
-//                    @Override
-//                    public Route.AsyncBuilder apply(PredicateSpec predicateSpec) {
-//                        return predicateSpec.path("/ribbon/**")
-//                                .uri("lb://M-COMSUMER-RIBBON")
-//                                .id("m-ribbon-one");
-//                    }
-//                })
-//                .route(new Function<PredicateSpec, Route.AsyncBuilder>() {
-//                    @Override
-//                    public Route.AsyncBuilder apply(PredicateSpec predicateSpec) {
-//                        return predicateSpec.path("/feign/**").filters(new Function<GatewayFilterSpec, UriSpec>() {
-//                            @Override
-//                            public UriSpec apply(GatewayFilterSpec gatewayFilterSpec) {
-//                                return gatewayFilterSpec.stripPrefix(1).filter(new MGateWayFilter());
-//                            }
-//                        }).uri("lb://M-COMSUMER-FEIGN")
-//                                .id("m-feign-two");
-//                    }
-//                }).build();
+//                // ribbon
+//                .route(ribbon -> ribbon.path("/ribbon/**")
+//                        .uri("lb://M-COMSUMER-RIBBON")
+//                        .id("m-ribbon-one"))
+//                //feign
+//                .route(feign -> feign.path("/feign/**").filters(a -> a.stripPrefix(1)
+//                        .filter(new MGateWayFilter()))
+//                        .uri("lb://M-COMSUMER-FEIGN")
+//                        .id("m-feign-two"))
+//                .build();
 //    }
-
-}
+////    @Bean
+////    public RouteLocator getRouteLocator(RouteLocatorBuilder builder) {
+////        return builder.routes()
+////                .route(new Function<PredicateSpec, Route.AsyncBuilder>() {
+////                    @Override
+////                    public Route.AsyncBuilder apply(PredicateSpec predicateSpec) {
+////                        return predicateSpec.path("/ribbon/**")
+////                                .uri("lb://M-COMSUMER-RIBBON")
+////                                .id("m-ribbon-one");
+////                    }
+////                })
+////                .route(new Function<PredicateSpec, Route.AsyncBuilder>() {
+////                    @Override
+////                    public Route.AsyncBuilder apply(PredicateSpec predicateSpec) {
+////                        return predicateSpec.path("/feign/**").filters(new Function<GatewayFilterSpec, UriSpec>() {
+////                            @Override
+////                            public UriSpec apply(GatewayFilterSpec gatewayFilterSpec) {
+////                                return gatewayFilterSpec.stripPrefix(1).filter(new MGateWayFilter());
+////                            }
+////                        }).uri("lb://M-COMSUMER-FEIGN")
+////                                .id("m-feign-two");
+////                    }
+////                }).build();
+////    }
+//
+//}
