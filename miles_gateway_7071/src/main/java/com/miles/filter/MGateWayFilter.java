b50a8cc15148c0b842fccaa39733e7262e0c0d31
ChinTingKun <awalsmile@qq.com>
2019-04-06 18:32:52 +0800

    gateway  自义Filter + 代码实现路  优势：灵活

--- miles_gateway_7071/src/main/java/com/miles/filter/MGateWayFilter.java
+++ miles_gateway_7071/src/main/java/com/miles/filter/MGateWayFilter.java
@@ -4,8 +4,12 @@ import com.miles.emun.GateWayOrder;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 /**
  * @ClassName MGateWayFilter
@@ -17,11 +21,16 @@ import reactor.core.publisher.Mono;
 public class MGateWayFilter implements GatewayFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        System.out.println("请求拦截" + exchange.getRequest().getURI().getPath());
+        ServerHttpRequest request = exchange.getRequest(); // request
+        Object xxx = exchange.getRequiredAttribute("xxx");// required request
+        Mono<MultiValueMap<String, String>> formData = exchange.getFormData();//表单数据
+        Map<String, Object> attributes = exchange.getAttributes(); //attributes
+
+        System.out.println("请求拦截" + request.getURI().getPath());
         return chain.filter(exchange);
     }
 
-    @Override
+    @Override // 过滤顺序，越小权重越高
     public int getOrder() {
         System.out.println("order is working");
         return GateWayOrder.IP_FILTER.getOrder();
